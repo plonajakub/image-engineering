4f3a6a8407a783c451ddac7411caf523cd3b418c
Jakub Plona <plonajakub@gmail.com>
2020-04-27 19:49:22 +0200

    lab4: add ex1 (simple ppm)

--- lab4/lab4-ppm.py
+++ lab4/lab4-ppm.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 import numpy
 
+img_width = 512
+img_height = 512
+img_px_size = img_width * img_height
+color_vector_rgb = [0, 255, 0]
+color_palette = 255
+ws = ' '
+cd_header = ws + str(img_width) + ws + str(img_height) + ws + str(color_palette) + ws
+
 # PPM file header
-ppm_ascii_header = ''  # TODO: implement
-ppm_binary_header = ''  # TODO: implement
+ppm_ascii_header = 'P3' + cd_header
+ppm_binary_header = 'P6' + cd_header
 
 # Image data
-image = numpy.array([], dtype=numpy.uint8)  # TODO: implement
+image = numpy.array([color_vector_rgb for i in range(img_width * img_height)], dtype=numpy.uint8)
 
 # Save the PPM image as an ASCII file
 with open('lab4-ascii.ppm', 'w') as fh:
