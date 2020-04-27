9602dd36236d07de21a088717b609549e225e938
Jakub Plona <plonajakub@gmail.com>
2020-04-27 21:39:44 +0200

    lab4: add ex2 (rainbow ppm)

--- lab4/lab4-ppm.py
+++ lab4/lab4-ppm.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import numpy
 
-img_width = 512
+colors_in_batch = 128
+batches = 7
+img_width = batches * colors_in_batch
 img_height = 512
 img_px_size = img_width * img_height
-color_vector_rgb = [0, 255, 0]
 color_palette = 255
 ws = ' '
 cd_header = ws + str(img_width) + ws + str(img_height) + ws + str(color_palette) + ws
@@ -14,7 +15,34 @@ ppm_ascii_header = 'P3' + cd_header
 ppm_binary_header = 'P6' + cd_header
 
 # Image data
-image = numpy.array([color_vector_rgb for i in range(img_width * img_height)], dtype=numpy.uint8)
+r_versor = numpy.array([1, 0, 0], dtype=numpy.uint8)
+g_versor = numpy.array([0, 1, 0], dtype=numpy.uint8)
+b_versor = numpy.array([0, 0, 1], dtype=numpy.uint8)
+step_size = 256 // colors_in_batch
+current_color = numpy.array([0, 0, 0], dtype=numpy.uint8)
+image = numpy.empty(shape=(img_height, img_width, 3), dtype=numpy.uint8)
+for batch_idx in range(batches):
+    current_op = None
+    if batch_idx == 0:
+        current_op = b_versor
+    elif batch_idx == 1:
+        current_op = g_versor
+    elif batch_idx == 2:
+        current_op = -b_versor
+    elif batch_idx == 3:
+        current_op = r_versor
+    elif batch_idx == 4:
+        current_op = -g_versor
+    elif batch_idx == 5:
+        current_op = b_versor
+    elif batch_idx == 6:
+        current_op = g_versor
+    for step_idx in range(colors_in_batch):
+        if step_idx == 0:
+            current_color += (step_size - 1) * current_op
+        else:
+            current_color += step_size * current_op
+        image[:, batch_idx * colors_in_batch + step_idx, :] = current_color
 
 # Save the PPM image as an ASCII file
 with open('lab4-ascii.ppm', 'w') as fh:
